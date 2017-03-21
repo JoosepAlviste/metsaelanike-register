f8543c9708813006b3b43ca91426f47e3fe6d14c
Joosep Alviste <joosep.alviste@gmail.com>
2017-03-21 16:12:14 +0200

    Template for showing one animal

--- forest_creatures/models.py
+++ forest_creatures/models.py
@@ -6,9 +6,15 @@ class Animal(models.Model):
     name = models.CharField(max_length=255)
     species = models.TextField()
 
+    def __str__(self):
+        return self.name
+
 
 class AnimalSighting(models.Model):
 
     animal = models.ForeignKey(Animal, on_delete=models.CASCADE)
     location = models.TextField()
     time = models.DateTimeField()
+
+    def __str__(self):
+        return 'Saw ' + self.animal.name + ' at ' + self.location + ' at ' + str(self.time)
--- forest_creatures/views.py
+++ forest_creatures/views.py
@@ -1,5 +1,20 @@
+from django.http import Http404
 from django.http import HttpResponse
+from django.shortcuts import render
+
+from forest_creatures.models import Animal
 
 
 def index(request):
+
     return HttpResponse('Hello World!')
+
+
+def show(request, animal_id):
+
+    try:
+        animal = Animal.objects.get(pk=animal_id)
+    except Animal.DoesNotExist:
+        raise Http404("Animal does not exist")
+
+    return render(request, 'forest_creatures/show.html', {'animal': animal})
