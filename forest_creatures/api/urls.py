32e1e6502372841ad98cebdd945e3c56232894a7
Joosep Alviste <joosep.alviste@gmail.com>
2017-03-29 13:52:19 +0300

    Fix a small url

--- forest_creatures/api/urls.py
+++ forest_creatures/api/urls.py
@@ -3,7 +3,7 @@ from django.conf.urls import url
 from forest_creatures.api import views
 
 urlpatterns = [
-    url(r'^/?$', views.AnimalList.as_view(), name='index'),
+    url(r'^$', views.AnimalList.as_view(), name='index'),
     url(r'^species/?$', views.SpeciesList.as_view(), name='all_species'),
     url(r'^species/(?P<pk>[0-9]+)/?$', views.SpeciesDetail.as_view(), name='one_species'),
     url(r'^species/(?P<species_id>[0-9]+)/animals/?$', views.AnimalListBySpecies.as_view(), name='animals_by_species'),
